630c2e0ebf015c8dbce8934feef87e08bf53250d
Darryl Yeng <darrylyeng@Desrees-MacBook-Pro.local>
2026-03-19 01:01:08 +0800

    Update unit test code for MarkCommand & MarkCommandParser

--- src/test/java/seedu/address/logic/commands/MarkCommandTest.java
+++ src/test/java/seedu/address/logic/commands/MarkCommandTest.java
@@ -17,6 +17,7 @@ import seedu.address.logic.Messages;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
+import seedu.address.model.person.DeliveryStatus;
 import seedu.address.model.person.Person;
 
 /**
@@ -30,13 +31,29 @@ public class MarkCommandTest {
     @Test
     public void execute_validIndexUnfilteredList_success() {
         Person personToMark = model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased());
-        MarkCommand command = new MarkCommand(INDEX_FIRST_PERSON);
+
+        // create updated person with new delivery status
+        Person updatedPerson = new Person(
+                personToMark.getName(),
+                personToMark.getPhone(),
+                personToMark.getEmail(),
+                personToMark.getAddress(),
+                personToMark.getBoxes(),
+                personToMark.getRemark(),
+                personToMark.getExpiryDate(),
+                DeliveryStatus.DELIVERED,
+                personToMark.getTags()
+        );
+
+        MarkCommand command = new MarkCommand(INDEX_FIRST_PERSON, DeliveryStatus.DELIVERED);
 
         String expectedMessage = String.format(
-                MarkCommand.MESSAGE_MARK_DELIVERED_SUCCESS,
-                Messages.format(personToMark));
+                MarkCommand.MESSAGE_MARK_DELIVERED,
+                Messages.format(updatedPerson));
 
         Model expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
+        expectedModel.setPerson(personToMark, updatedPerson);
+        expectedModel.updateFilteredPersonList(Model.PREDICATE_SHOW_ALL_PERSONS);
 
         assertCommandSuccess(command, model, expectedMessage, expectedModel);
     }
@@ -44,29 +61,41 @@ public class MarkCommandTest {
     @Test
     public void execute_invalidIndexUnfilteredList_throwsCommandException() {
         Index outOfBoundIndex = Index.fromOneBased(model.getFilteredPersonList().size() + 1);
-        MarkCommand command = new MarkCommand(outOfBoundIndex);
+        MarkCommand command = new MarkCommand(outOfBoundIndex, DeliveryStatus.DELIVERED);
 
         assertCommandFailure(command, model, Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
     }
 
-    /*
-    !!! will be implemented once delivery status field is connected
-
     @Test
-    public void execute_validIndexFilteredList_success() throws Exception {
+    public void execute_validIndexFilteredList_success() {
         showPersonAtIndex(model, INDEX_FIRST_PERSON);
 
         Person personToMark = model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased());
-        MarkCommand command = new MarkCommand(INDEX_FIRST_PERSON);
+
+        Person updatedPerson = new Person(
+                personToMark.getName(),
+                personToMark.getPhone(),
+                personToMark.getEmail(),
+                personToMark.getAddress(),
+                personToMark.getBoxes(),
+                personToMark.getRemark(),
+                personToMark.getExpiryDate(),
+                DeliveryStatus.DELIVERED,
+                personToMark.getTags()
+        );
+
+        MarkCommand command = new MarkCommand(INDEX_FIRST_PERSON, DeliveryStatus.DELIVERED);
 
         String expectedMessage = String.format(
-                MarkCommand.MESSAGE_MARK_DELIVERED_SUCCESS,
-                Messages.format(personToMark));
+                MarkCommand.MESSAGE_MARK_DELIVERED,
+                Messages.format(updatedPerson));
 
         Model expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
+        expectedModel.setPerson(personToMark, updatedPerson);
+        expectedModel.updateFilteredPersonList(Model.PREDICATE_SHOW_ALL_PERSONS);
+
         assertCommandSuccess(command, model, expectedMessage, expectedModel);
     }
-    */
 
     @Test
     public void execute_invalidIndexFilteredList_throwsCommandException() {
@@ -77,21 +106,22 @@ public class MarkCommandTest {
         assertTrue(outOfBoundIndex.getZeroBased()
                 < model.getAddressBook().getPersonList().size());
 
-        MarkCommand command = new MarkCommand(outOfBoundIndex);
+        MarkCommand command = new MarkCommand(outOfBoundIndex, DeliveryStatus.DELIVERED);
 
         assertCommandFailure(command, model, Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
     }
 
     @Test
     public void equals() {
-        MarkCommand firstCommand = new MarkCommand(INDEX_FIRST_PERSON);
-        MarkCommand secondCommand = new MarkCommand(INDEX_SECOND_PERSON);
+        MarkCommand firstCommand = new MarkCommand(INDEX_FIRST_PERSON, DeliveryStatus.DELIVERED);
+        MarkCommand secondCommand = new MarkCommand(INDEX_SECOND_PERSON, DeliveryStatus.DELIVERED);
+        MarkCommand anotherStatusCommand = new MarkCommand(INDEX_FIRST_PERSON, DeliveryStatus.PACKED);
 
         // same object -> true
         assertTrue(firstCommand.equals(firstCommand));
 
         // same values -> true
-        MarkCommand firstCommandCopy = new MarkCommand(INDEX_FIRST_PERSON);
+        MarkCommand firstCommandCopy = new MarkCommand(INDEX_FIRST_PERSON, DeliveryStatus.DELIVERED);
         assertTrue(firstCommand.equals(firstCommandCopy));
 
         // different types -> false
@@ -102,14 +132,18 @@ public class MarkCommandTest {
 
         // different index -> false
         assertFalse(firstCommand.equals(secondCommand));
+
+        // different delivery status -> false
+        assertFalse(firstCommand.equals(anotherStatusCommand));
     }
 
     @Test
     public void toStringMethod() {
         Index targetIndex = Index.fromOneBased(1);
-        MarkCommand command = new MarkCommand(targetIndex);
+        MarkCommand command = new MarkCommand(targetIndex, DeliveryStatus.DELIVERED);
         String expected = MarkCommand.class.getCanonicalName()
-                + "{targetIndex=" + targetIndex + "}";
+                + "{targetIndex=" + targetIndex
+                + ", deliveryStatus=" + DeliveryStatus.DELIVERED + "}";
         assertEquals(expected, command.toString());
     }
 }
--- src/test/java/seedu/address/logic/parser/MarkCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/MarkCommandParserTest.java
@@ -8,22 +8,40 @@ import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 import org.junit.jupiter.api.Test;
 
 import seedu.address.logic.commands.MarkCommand;
+import seedu.address.model.person.DeliveryStatus;
 
 /**
  * Tests for MarkCommandParser.
  */
 public class MarkCommandParserTest {
 
-    private MarkCommandParser parser = new MarkCommandParser();
+    private final MarkCommandParser parser = new MarkCommandParser();
 
     @Test
     public void parse_validArgs_returnsMarkCommand() {
-        assertParseSuccess(parser, "1", new MarkCommand(INDEX_FIRST_PERSON));
+        // Updated to include delivery status
+        assertParseSuccess(parser, "1 DELIVERED",
+                new MarkCommand(INDEX_FIRST_PERSON, DeliveryStatus.DELIVERED));
+
+        assertParseSuccess(parser, "1 PACKED",
+                new MarkCommand(INDEX_FIRST_PERSON, DeliveryStatus.PACKED));
+
+        assertParseSuccess(parser, "1 PENDING",
+                new MarkCommand(INDEX_FIRST_PERSON, DeliveryStatus.PENDING));
     }
 
     @Test
     public void parse_invalidArgs_throwsParseException() {
-        assertParseFailure(parser, "a",
+        // invalid index
+        assertParseFailure(parser, "a DELIVERED",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, MarkCommand.MESSAGE_USAGE));
+
+        // missing delivery status
+        assertParseFailure(parser, "1",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, MarkCommand.MESSAGE_USAGE));
+
+        // invalid delivery status
+        assertParseFailure(parser, "1 INVALIDSTATUS",
                 String.format(MESSAGE_INVALID_COMMAND_FORMAT, MarkCommand.MESSAGE_USAGE));
     }
 }
